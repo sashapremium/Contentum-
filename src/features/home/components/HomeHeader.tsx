2ff596e4c3c8e604f8de748fa16e6a4ae91227e4
Petr Kulikov <p.kulikov.dev@gmail.com>
2025-11-30 00:26:46 +0500

    Убраны тестовые вызовы алертов

--- src/features/home/components/HomeHeader.tsx
+++ src/features/home/components/HomeHeader.tsx
@@ -1,13 +1,8 @@
 import { Button } from '@/components/ui/button';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { Ellipsis } from 'lucide-react';
-import { useInfo, useError, useSuccess } from '@/hooks/useToast';
 
 export const HomeHeader = () => {
-  const i = useInfo();
-  const e = useError();
-  const s = useSuccess();
-
   return (
     <header className="border-b p-3 bg-background grid grid-cols-[auto_1fr_auto] items-center gap-3">
       <SidebarTrigger />
@@ -15,27 +10,6 @@ export const HomeHeader = () => {
       <h1 className="font-medium text-lg truncate">Новый чат</h1>
 
       <Button variant="ghost" size="icon-sm">
-        <Ellipsis />
-      </Button>
-      <Button
-        variant="ghost"
-        size="icon-sm"
-        onClick={() => s('Message in toast component')}
-      >
-        <Ellipsis />
-      </Button>
-      <Button
-        variant="ghost"
-        size="icon-sm"
-        onClick={() => i('Message in toast component')}
-      >
-        <Ellipsis />
-      </Button>
-      <Button
-        variant="ghost"
-        size="icon-sm"
-        onClick={() => e('Message in toast component')}
-      >
         <Ellipsis />
       </Button>
     </header>
