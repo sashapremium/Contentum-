38e889d1c2e1986c14c32b78383797fb8738d2e3
Petr Kulikov <p.kulikov.dev@gmail.com>
2025-11-30 00:57:39 +0500

    Добавлены опции для пользователя

--- src/features/home/components/sidebar/SidebarFooter.tsx
+++ src/features/home/components/sidebar/SidebarFooter.tsx
@@ -1,10 +1,18 @@
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
   SidebarFooter as UISidebarFooter,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from '@/components/ui/sidebar';
+import { LogOut, Settings } from 'lucide-react';
 
 const USER = {
   name: 'Куликов Пётр Сергеевич',
@@ -16,18 +24,38 @@ export const SidebarFooter = () => {
     <UISidebarFooter>
       <SidebarMenu>
         <SidebarMenuItem>
-          <SidebarMenuButton
-            size="lg"
-            className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-          >
-            <Avatar className="h-8 w-8 rounded-lg">
-              <AvatarFallback className="rounded-lg">PK</AvatarFallback>
-            </Avatar>
-            <div className="grid flex-1 text-left text-sm leading-tight">
-              <span className="truncate font-medium">{USER.name}</span>
-              <span className="truncate text-xs">{USER.email}</span>
-            </div>
-          </SidebarMenuButton>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton
+                size="lg"
+                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+              >
+                <Avatar className="h-8 w-8 rounded-lg">
+                  <AvatarFallback className="rounded-lg">PK</AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">{USER.name}</span>
+                  <span className="truncate text-xs">{USER.email}</span>
+                </div>
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+              side="top"
+              align="center"
+              sideOffset={4}
+            >
+              <DropdownMenuItem>
+                <Settings />
+                Настройки
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <LogOut />
+                Выйти
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </SidebarMenuItem>
       </SidebarMenu>
     </UISidebarFooter>
