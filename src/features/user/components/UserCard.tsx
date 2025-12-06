dbf200886f1db06cee3f0750b7e63f2b27f80056
Petr Kulikov <p.kulikov.dev@gmail.com>
2025-12-06 19:24:21 +0500

    Добавлены скелетоны для имени пользователя

--- src/features/user/components/UserCard.tsx
+++ src/features/user/components/UserCard.tsx
@@ -7,6 +7,7 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { SidebarMenuButton } from '@/components/ui/sidebar';
+import { Skeleton } from '@/components/ui/skeleton';
 import { LogoutDialog } from '@/features/home/components/sidebar/LogoutDialog';
 import { useTodo } from '@/hooks/useToast';
 import { User, Settings, LogOut } from 'lucide-react';
@@ -21,6 +22,8 @@ export const UserCard = () => {
   const [open, setOpen] = useState(false);
   const t = useTodo();
 
+  const loading = true;
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -33,9 +36,18 @@ export const UserCard = () => {
               <User />
             </AvatarFallback>
           </Avatar>
-          <div className="grid flex-1 text-left text-sm leading-tight">
-            <span className="truncate font-medium">{USER.name}</span>
-            <span className="truncate text-xs">{USER.email}</span>
+          <div className="grid flex-1 text-left text-sm leading-tight gap-1">
+            {loading ? (
+              <Skeleton className="w-[180px] h-[17.5px]" />
+            ) : (
+              <span className="truncate font-medium">{USER.name}</span>
+            )}
+
+            {loading ? (
+              <Skeleton className="w-[150px] h-[14px]" />
+            ) : (
+              <span className="truncate text-xs">{USER.email}</span>
+            )}
           </div>
         </SidebarMenuButton>
       </DropdownMenuTrigger>
