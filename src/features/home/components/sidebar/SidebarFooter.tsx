7205caded1be6b9e4be517361e3fcfda5df7f27a
Petr Kulikov <p.kulikov.dev@gmail.com>
2025-12-06 19:15:51 +0500

    Добавлен компонент для инфы пользователя

--- src/features/home/components/sidebar/SidebarFooter.tsx
+++ src/features/home/components/sidebar/SidebarFooter.tsx
@@ -1,73 +1,18 @@
-import { Avatar, AvatarFallback } from '@/components/ui/avatar';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 import {
   SidebarFooter as UISidebarFooter,
   SidebarMenu,
-  SidebarMenuButton,
   SidebarMenuItem,
 } from '@/components/ui/sidebar';
-import { LogOut, Settings, User } from 'lucide-react';
-import { useState } from 'react';
-import { LogoutDialog } from './LogoutDialog';
-import { useTodo } from '@/hooks/useToast';
-
-const USER = {
-  name: 'Куликов Глубокослав Сергеевич',
-  email: 'p.kulikov.dev@gmaiффффффффффффl.com',
-};
+import { UserCard } from '@/features/user/components/UserCard';
 
 export const SidebarFooter = () => {
-  const [open, setOpen] = useState(false);
-  const t = useTodo();
-
   return (
     <UISidebarFooter>
       <SidebarMenu>
         <SidebarMenuItem>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <SidebarMenuButton
-                size="lg"
-                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-              >
-                <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarFallback className="rounded-lg">
-                    <User />
-                  </AvatarFallback>
-                </Avatar>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{USER.name}</span>
-                  <span className="truncate text-xs">{USER.email}</span>
-                </div>
-              </SidebarMenuButton>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent
-              className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-              side="top"
-              align="center"
-              sideOffset={4}
-            >
-              <DropdownMenuItem onClick={() => t('Настройки')}>
-                <Settings />
-                Настройки
-              </DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem onClick={() => setOpen(true)}>
-                <LogOut />
-                Выйти
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <UserCard />
         </SidebarMenuItem>
       </SidebarMenu>
-
-      <LogoutDialog open={open} onOpenChange={setOpen} />
     </UISidebarFooter>
   );
 };
---
+++ src/features/user/components/UserCard.tsx
@@ -0,0 +1,62 @@
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { SidebarMenuButton } from '@/components/ui/sidebar';
+import { LogoutDialog } from '@/features/home/components/sidebar/LogoutDialog';
+import { useTodo } from '@/hooks/useToast';
+import { User, Settings, LogOut } from 'lucide-react';
+import { useState } from 'react';
+
+const USER = {
+  name: 'Куликов Глубокослав Сергеевич',
+  email: 'p.kulikov.dev@gmaiффффффффффффl.com',
+};
+
+export const UserCard = () => {
+  const [open, setOpen] = useState(false);
+  const t = useTodo();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <SidebarMenuButton
+          size="lg"
+          className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+        >
+          <Avatar className="h-8 w-8 rounded-lg">
+            <AvatarFallback className="rounded-lg">
+              <User />
+            </AvatarFallback>
+          </Avatar>
+          <div className="grid flex-1 text-left text-sm leading-tight">
+            <span className="truncate font-medium">{USER.name}</span>
+            <span className="truncate text-xs">{USER.email}</span>
+          </div>
+        </SidebarMenuButton>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+        side="top"
+        align="center"
+        sideOffset={4}
+      >
+        <DropdownMenuItem onClick={() => t('Настройки')}>
+          <Settings />
+          Настройки
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => setOpen(true)}>
+          <LogOut />
+          Выйти
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+
+      <LogoutDialog open={open} onOpenChange={setOpen} />
+    </DropdownMenu>
+  );
+};
