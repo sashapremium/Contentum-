82f9db7870049348456722319f92010b76dda80b
Petr Kulikov <p.kulikov.dev@gmail.com>
2025-12-09 02:23:12 +0500

    Обновлены действия с сообщениями

---
+++ src/features/messages/components/MessageActions/CopyAction.tsx
@@ -0,0 +1,48 @@
+import { Button } from '@/components/ui/button';
+import type { MessageActionsProps } from './MessageActions';
+import { Check, Copy } from 'lucide-react';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { useEffect, useState } from 'react';
+
+export const CopyAction = ({ message }: MessageActionsProps) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    setCopied(true);
+    const text = message.content.info as string;
+    navigator.clipboard.writeText(text);
+  };
+
+  useEffect(() => {
+    if (!copied) return;
+
+    const timer = setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [copied]);
+
+  return (
+    <Tooltip>
+      <TooltipTrigger>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="h-7 w-7"
+          onClick={handleCopy}
+        >
+          {copied ? <Check /> : <Copy />}
+        </Button>
+      </TooltipTrigger>
+
+      <TooltipContent showArrow={false} side="bottom" className="p-1">
+        <span className="font-medium">Копировать</span>
+      </TooltipContent>
+    </Tooltip>
+  );
+};
---
+++ src/features/messages/components/MessageActions/DownloadAction.tsx
@@ -0,0 +1,27 @@
+import { Button } from '@/components/ui/button';
+import type { MessageActionsProps } from './MessageActions';
+import type { ImageInfo } from '../../types/messages.types';
+import { Download } from 'lucide-react';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+
+export const DowloadAction = ({ message }: MessageActionsProps) => {
+  return (
+    <Tooltip>
+      <TooltipTrigger>
+        <Button variant="ghost" size="icon" className="h-7 w-7">
+          <a download href={(message.content.info as ImageInfo).download_url}>
+            <Download />
+          </a>
+        </Button>
+      </TooltipTrigger>
+
+      <TooltipContent showArrow={false} side="bottom" className="p-1">
+        <span className="font-medium">Скачать</span>
+      </TooltipContent>
+    </Tooltip>
+  );
+};
