4f5616cb162432c002222d47de8c9eaeaf37f79e
Petr Kulikov <p.kulikov.dev@gmail.com>
2026-03-01 20:46:11 +0500

    Правки

--- src/features/user/components/UserCard.tsx
+++ src/features/user/components/UserCard.tsx
@@ -14,17 +14,6 @@ import { mapApiError } from '@/lib/apiErrorMapper';
 export const UserCard = () => {
   const { data: user, isLoading, isError, error } = useUserMeQuery();
 
-  if (isError) {
-    return (
-      <Error
-        description={mapApiError(
-          error,
-          'Ошибка при загрузке данных о пользователе.'
-        )}
-      />
-    );
-  }
-
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -32,28 +21,39 @@ export const UserCard = () => {
           size="lg"
           className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
         >
-          <Avatar className="h-8 w-8 rounded-lg">
-            {isLoading ? (
-              <Skeleton className="h-8 w-8 rounded-full" />
-            ) : (
-              <AvatarFallback className="rounded-lg">
-                {user?.role === 'EMPLOYEE' ? <User /> : <UserCog />}
-              </AvatarFallback>
-            )}
-          </Avatar>
-          <div className="grid flex-1 text-left text-sm leading-tight gap-1">
-            {isLoading ? (
-              <Skeleton className="w-[180px] h-[17.5px]" />
-            ) : (
-              <span className="truncate font-medium">{user?.fullName}</span>
-            )}
+          {isError ? (
+            <Error
+              description={mapApiError(
+                error,
+                'Ошибка при загрузке данных о пользователе.',
+              )}
+            />
+          ) : (
+            <>
+              <Avatar className="h-8 w-8 rounded-lg">
+                {isLoading ? (
+                  <Skeleton className="h-8 w-8 rounded-full" />
+                ) : (
+                  <AvatarFallback className="rounded-lg">
+                    {user?.role === 'EMPLOYEE' ? <User /> : <UserCog />}
+                  </AvatarFallback>
+                )}
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight gap-1">
+                {isLoading ? (
+                  <Skeleton className="w-[180px] h-[17.5px]" />
+                ) : (
+                  <span className="truncate font-medium">{user?.fullName}</span>
+                )}
 
-            {isLoading ? (
-              <Skeleton className="w-[150px] h-[14px]" />
-            ) : (
-              <span className="truncate text-xs">{user?.email}</span>
-            )}
-          </div>
+                {isLoading ? (
+                  <Skeleton className="w-[150px] h-[14px]" />
+                ) : (
+                  <span className="truncate text-xs">{user?.email}</span>
+                )}
+              </div>
+            </>
+          )}
         </SidebarMenuButton>
       </DropdownMenuTrigger>
 
