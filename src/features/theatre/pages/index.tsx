488b094bacfa1e7433b9d90cfb76cd43dde770cd
Petr Kulikov <p.kulikov.dev@gmail.com>
2026-05-01 15:38:44 +0500

    Добавлена редирект на шаблоны

--- src/features/theatre/pages/index.tsx
+++ src/features/theatre/pages/index.tsx
@@ -1,12 +1,19 @@
-import { THEATRE_TITLE } from '@/app/router/routes';
+import { THEATRE_TEMPLATE_CREATE, THEATRE_TITLE } from '@/app/router/routes';
 import { PageHeading } from '@/components/shared/PageHeading';
 import { PageWrapper } from '@/components/shared/PageWrapper';
+import { Link } from 'react-router';
+import { Button } from '@/components/ui/button';
+import { FilePlus } from 'lucide-react';
 
 export const TheatrePage = () => {
   return (
-    <PageWrapper
-      wide
-      header={<PageHeading>{THEATRE_TITLE}</PageHeading>}
-    ></PageWrapper>
+    <PageWrapper wide header={<PageHeading>{THEATRE_TITLE}</PageHeading>}>
+      <Button asChild>
+        <Link to={THEATRE_TEMPLATE_CREATE}>
+          <FilePlus />
+          Создать шаблон
+        </Link>
+      </Button>
+    </PageWrapper>
   );
 };
