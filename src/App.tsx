6efa4edd4e32d846448055659f1b487f11d66a2e
Petr Kulikov <p.kulikov.dev@gmail.com>
2026-06-12 17:33:18 +0500

    Удалены моки

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClientProvider } from '@tanstack/react-query';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AppRouter } from '@/app/router';
 import { AuthProvider } from './features/auth/providers/AuthProvider';
 import { Toaster } from '@/components/ui/sonner';
@@ -12,7 +11,6 @@ export default function App() {
       <QueryClientProvider client={queryClient}>
         <ThemeProvider>
           <Toaster />
-          {/* <ReactQueryDevtools initialIsOpen={false} /> */}
           <AppRouter />
         </ThemeProvider>
       </QueryClientProvider>
